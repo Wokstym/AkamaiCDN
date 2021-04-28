6e4bf8c0ac36e015899fcc4adee2cf6d07ca27b1
WikKam <kamykikamienie@gmail.com>
2021-04-28 12:51:08 +0200

    zunifikowanie sekcji

---
+++ FrontendAkamaiCdn/src/utils/index.js
@@ -0,0 +1,11 @@
+export function groupBy(arr, field){
+    return Object.entries(
+        arr.reduce((reducer, next) => {
+            reducer[next[field]] = reducer[next[field]] || [];
+            reducer[next[field]].push({
+                ...next,
+            });
+            return reducer;
+        }, {})
+    );
+}
